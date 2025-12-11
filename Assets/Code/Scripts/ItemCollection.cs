402f1ff15554829c43c0c3eafa1363803d19df88
Martino Pagliarani <martipaglia02@gmail.com>
2025-12-11 18:40:16 +0100

    Template for ItemCollection, template for display area for tokens and cards

---
+++ Assets/Code/Scripts/ItemCollection.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ItemCollection : MonoBehaviour
+{
+    // Generic class that will be used by Deck, ResourceTokenBag, CardsInPlayArea, ResourceTokenArea.
+    // It will use the instance pattern and instance will be removed from those classes
+    // Instance and Awake singleton declaration
+    // Update with raycast to check clicked, OnCollectionClicked, DrawItem, Shuffle.
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
