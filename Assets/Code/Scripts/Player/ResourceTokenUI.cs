280cd3b5a9cabf3de759c962172d2369c690f8c4
Martino Pagliarani <martipaglia02@gmail.com>
2026-01-08 20:02:25 +0100

    ResourceTokenUI function complete with relative unity editor implementation

--- Assets/Code/Scripts/Player/ResourceTokenUI.cs
+++ Assets/Code/Scripts/Player/ResourceTokenUI.cs
@@ -1,9 +1,19 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using static ResourceToken;
 
 public class ResourceTokenUI : MonoBehaviour
 {
     private Transform resourceTokenContainer;
+    [Header("Amount Labels")]
+    [SerializeField] private TextMeshProUGUI ductTapeText;
+    [SerializeField] private TextMeshProUGUI glueText;
+    [SerializeField] private TextMeshProUGUI wireText;
+    [SerializeField] private TextMeshProUGUI fabricText;
+    [SerializeField] private TextMeshProUGUI plasticText;
+    [SerializeField] private TextMeshProUGUI glitterText;
+
 
     void Awake()
     {
@@ -12,9 +22,6 @@ public class ResourceTokenUI : MonoBehaviour
 
     public void UpdateResourceTokenUI(List<ResourceToken> resourceTokens)
     {
-        // TODO: Just update the numbers on the UI without the complicated animation since it's not really needed (the tokens are all the same except for the type that also determines its color/texture)
-        // It just needs to reparent the tokens to the UI and to disable everything 3D-wise
-        // TODO: Metti a posto la UI su unity della hand e update qua
         foreach (ResourceToken resourceToken in resourceTokens)
         {
             // Disable 3D rendering
@@ -27,6 +34,28 @@ public class ResourceTokenUI : MonoBehaviour
             // Reparent to resource token container (Canvas)
             RectTransform tokenRect = resourceToken.GetComponent<RectTransform>();
             if (tokenRect != null) tokenRect.SetParent(resourceTokenContainer);
+
+            // Update the corresponding amount label
+            int ductTape = 0, glue = 0, wire = 0, fabric = 0, plastic = 0, glitter = 0;
+
+            foreach (var token in resourceTokens) {
+                switch (token.resourceTokenType)
+                {
+                    case ResourceTokenType.DuctTape: ductTape++; break;
+                    case ResourceTokenType.Glue: glue++; break;
+                    case ResourceTokenType.Wire: wire++; break;
+                    case ResourceTokenType.Fabric: fabric++; break;
+                    case ResourceTokenType.Plastic: plastic++; break;
+                    case ResourceTokenType.Glitter: glitter++; break;
+                }
+            }
+
+            if (ductTapeText != null) ductTapeText.text = ductTape.ToString();
+            if (glueText != null) glueText.text = glue.ToString();
+            if (wireText != null) wireText.text = wire.ToString();
+            if (fabricText != null) fabricText.text = fabric.ToString();
+            if (plasticText != null) plasticText.text = plastic.ToString();
+            if (glitterText != null) glitterText.text = glitter.ToString();
         }
     }
 }
