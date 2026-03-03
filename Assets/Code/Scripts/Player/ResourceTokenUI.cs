4540eaa5c48250eb7cf5c0ee1588d5fdda964786
Martino Pagliarani <martipaglia02@gmail.com>
2026-03-03 19:34:47 +0100

    Enhance ResourceTokenUI with background color support and glitter effect; update ResourceToken for color management

--- Assets/Code/Scripts/Player/ResourceTokenUI.cs
+++ Assets/Code/Scripts/Player/ResourceTokenUI.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using static ResourceToken;
 
 public class ResourceTokenUI : MonoBehaviour
 {
     private Transform resourceTokenContainer;
+
     [Header("Amount Labels")]
     [SerializeField] private TextMeshProUGUI ductTapeText;
     [SerializeField] private TextMeshProUGUI glueText;
@@ -16,9 +19,81 @@ public class ResourceTokenUI : MonoBehaviour
 
     private readonly List<ResourceToken> processedTokens = new List<ResourceToken>();
 
+    [Header("Background Images")]
+    [SerializeField] private Image ductTapeBg;
+    [SerializeField] private Image glueBg;
+    [SerializeField] private Image wireBg;
+    [SerializeField] private Image fabricBg;
+    [SerializeField] private Image plasticBg;
+    [SerializeField] private Image glitterBg;
+
+    [Header("Glitter Rainbow")]
+    [SerializeField] private float glitterCycleSeconds = 2.0f;
+
+    private Color[] glitterPalette;
+    private float glitterTimer;
+
     void Awake()
     {
         resourceTokenContainer = GetComponent<Transform>();
+        ApplyTypeColorsToBackgrounds();
+        BuildGlitterPalette();
+    }
+
+    private void Update()
+    {
+        UpdateGlitterBackground();
+    }
+
+    private void ApplyTypeColorsToBackgrounds()
+    {
+        if (ductTapeBg != null) ductTapeBg.color = GetColorForType(ResourceTokenType.DuctTape);
+        if (glueBg != null) glueBg.color = GetColorForType(ResourceTokenType.Glue);
+        if (wireBg != null) wireBg.color = GetColorForType(ResourceTokenType.Wire);
+        if (fabricBg != null) fabricBg.color = GetColorForType(ResourceTokenType.Fabric);
+        if (plasticBg != null) plasticBg.color = GetColorForType(ResourceTokenType.Plastic);
+    }
+
+    private void BuildGlitterPalette()
+    {
+        glitterPalette = new[]
+        {
+            GetColorForType(ResourceTokenType.DuctTape),
+            GetColorForType(ResourceTokenType.Glue),
+            GetColorForType(ResourceTokenType.Wire),
+            GetColorForType(ResourceTokenType.Fabric),
+            GetColorForType(ResourceTokenType.Plastic)
+        };
+    }
+
+    private void UpdateGlitterBackground()
+    {
+        if (glitterBg == null || glitterPalette == null || glitterPalette.Length == 0)
+        {
+            return;
+        }
+
+        if (glitterCycleSeconds <= 0.01f)
+        {
+            glitterBg.color = glitterPalette[0];
+            return;
+        }
+
+        glitterTimer += Time.deltaTime;
+        float progress = Mathf.Repeat(glitterTimer / glitterCycleSeconds, 1f);
+        glitterBg.color = EvaluateGlitterColor(progress);
+    }
+
+    private Color EvaluateGlitterColor(float progress)
+    {
+        int paletteCount = glitterPalette.Length;
+        float scaled = progress * paletteCount;
+
+        int fromIndex = Mathf.FloorToInt(scaled) % paletteCount;
+        int toIndex = (fromIndex + 1) % paletteCount;
+        float blend = scaled - Mathf.Floor(scaled);
+
+        return Color.Lerp(glitterPalette[fromIndex], glitterPalette[toIndex], blend);
     }
 
     public void UpdateResourceTokenUI(List<ResourceToken> resourceTokens)
@@ -26,47 +101,45 @@ public class ResourceTokenUI : MonoBehaviour
         // First, ensure all tokens are properly set up for UI
         foreach (ResourceToken resourceToken in resourceTokens)
         {
-            if (!processedTokens.Contains(resourceToken))
-            {
-                // Disable 3D rendering
-                MeshRenderer meshRenderer = resourceToken.GetComponent<MeshRenderer>();
-                if (meshRenderer != null) meshRenderer.enabled = false;
-
-                Collider collider = resourceToken.GetComponent<Collider>();
-                if (collider != null) collider.enabled = false;
-
-                // Reparent to resource token container (Canvas)
-                if (resourceToken.TryGetComponent<RectTransform>(out var tokenRect))
-                {
-                    tokenRect.SetParent(resourceTokenContainer);
-                }
-
-                processedTokens.Add(resourceToken);
-            }
+            HideTokenForUI(resourceToken);
         }
+        AssignTokensCount(resourceTokens);
+    }
 
-        // Now count tokens by type
-        int ductTape = 0, glue = 0, wire = 0, fabric = 0, plastic = 0, glitter = 0;
+    private void HideTokenForUI(ResourceToken resourceToken)
+    {
+        if (processedTokens.Contains(resourceToken)) return;
 
-        foreach (var token in resourceTokens)
+        // Disable 3D rendering
+        if (resourceToken.TryGetComponent<MeshRenderer>(out var meshRenderer)) meshRenderer.enabled = false;
+
+        // Disable collider
+        if (resourceToken.TryGetComponent<Collider>(out var collider)) collider.enabled = false;
+
+        // Reparent to resource token container (Canvas)
+        if (resourceToken.TryGetComponent<RectTransform>(out var tokenRect))
         {
-            switch (token.resourceTokenType)
-            {
-                case ResourceTokenType.DuctTape: ductTape++; break;
-                case ResourceTokenType.Glue: glue++; break;
-                case ResourceTokenType.Wire: wire++; break;
-                case ResourceTokenType.Fabric: fabric++; break;
-                case ResourceTokenType.Plastic: plastic++; break;
-                case ResourceTokenType.Glitter: glitter++; break;
-            }
+            tokenRect.SetParent(resourceTokenContainer);
         }
+        processedTokens.Add(resourceToken);
+    }
 
-        // Update UI labels
-        if (ductTapeText != null) ductTapeText.text = ductTape.ToString();
-        if (glueText != null) glueText.text = glue.ToString();
-        if (wireText != null) wireText.text = wire.ToString();
-        if (fabricText != null) fabricText.text = fabric.ToString();
-        if (plasticText != null) plasticText.text = plastic.ToString();
-        if (glitterText != null) glitterText.text = glitter.ToString();
+    private void AssignTokensCount(List<ResourceToken> resourceTokens)
+    {
+        List<(ResourceTokenType, TextMeshProUGUI)> tokenCounts = new List<(ResourceTokenType, TextMeshProUGUI)>
+        {
+            (ResourceTokenType.DuctTape, ductTapeText),
+            (ResourceTokenType.Glue, glueText),
+            (ResourceTokenType.Wire, wireText),
+            (ResourceTokenType.Fabric, fabricText),
+            (ResourceTokenType.Plastic, plasticText),
+            (ResourceTokenType.Glitter, glitterText)
+        };
+
+        foreach (var (tokenType, textComponent) in tokenCounts)
+        {
+            int count = resourceTokens.Count(t => t.resourceTokenType == tokenType);
+            textComponent.text = count.ToString();
+        }
     }
 }
--- Assets/Code/Scripts/ResourceToken/ResourceToken.cs
+++ Assets/Code/Scripts/ResourceToken/ResourceToken.cs
@@ -1,7 +1,15 @@
+using System.Net;
 using UnityEngine;
 
 public class ResourceToken : MonoBehaviour
 {
+    public static readonly Color Grey = Color.grey;
+    public static readonly Color Blue = Color.blue;
+    public static readonly Color Copper = new Color(184f / 255f, 115f / 255f, 51f / 255f);
+    public static readonly Color Green = Color.green;
+    public static readonly Color Red = Color.red;
+
+    public static readonly Color GlitterColor;
 
     public enum ResourceTokenType
     {
@@ -13,4 +21,25 @@ public class ResourceToken : MonoBehaviour
         Glitter
     }
     public ResourceTokenType resourceTokenType;
+
+    public Color resourceTokenColor;
+
+    private void Awake()
+    {
+        resourceTokenColor = GetColorForType(resourceTokenType);
+    }
+
+    public static Color GetColorForType(ResourceTokenType type)
+    {
+        return type switch
+        {
+            ResourceTokenType.DuctTape => new Color(Grey.r, Grey.g, Grey.b),
+            ResourceTokenType.Glue => new Color(Blue.r, Blue.g, Blue.b),
+            ResourceTokenType.Wire => new Color(Copper.r, Copper.g, Copper.b),
+            ResourceTokenType.Fabric => new Color(Green.r, Green.g, Green.b),
+            ResourceTokenType.Plastic => new Color(Red.r, Red.g, Red.b),
+            ResourceTokenType.Glitter => GlitterColor,
+            _ => Color.magenta,
+        };
+    }
 }
